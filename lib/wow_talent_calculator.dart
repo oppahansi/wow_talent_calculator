bd65b23aeab5dc503927c5a1ffcd0c5541f0a573
Alexander Schellenberg <oppahansi@gmail.com>
2021-10-26 14:54:31 +0200

    Exporting constants

--- lib/wow_talent_calculator.dart
+++ lib/wow_talent_calculator.dart
@@ -3,3 +3,4 @@ library wow_talent_calculator;
 export 'src/wow_talent_calculator.dart';
 export 'src/models/talent_tree_position.dart';
 export 'src/exceptions/talent_calculator_exception.dart';
+export 'src/utils/constants.dart';
