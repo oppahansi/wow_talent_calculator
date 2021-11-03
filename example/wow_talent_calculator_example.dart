84f4d42a9149de3d6e4dce6d734c76c3d9ae69b1
Alexander Schellenberg <oppahansi@gmail.com>
2021-11-04 00:57:50 +0100

    Updated calculator conditions again

--- example/wow_talent_calculator_example.dart
+++ example/wow_talent_calculator_example.dart
@@ -3,15 +3,29 @@ import 'package:wow_talent_calculator/wow_talent_calculator.dart';
 void main() {
   var wtc = WowTalentCalculator(expansionId: 0, charClassId: 0);
 
-  for (int i = 0; i < 5; i++) {
+  wtc = WowTalentCalculator();
+
+  for (int i = 0; i < 4; i++) {
     wtc.investPointAt(0, 0);
   }
 
   wtc.investPointAt(0, 1);
+  wtc.investPointAt(0, 2);
 
-  wtc.resetAll();
+  for (int i = 0; i < 3; i++) {
+    wtc.investPointAt(0, 4);
+  }
 
-  var availabilities = wtc.getAvailabilityStates;
+  wtc.printSpec(0);
+
+  wtc.investPointAt(0, 0);
+  wtc.investPointAt(0, 8);
+
+  wtc.printSpec(0);
+
+  int talentPoints = wtc.getInvestedPointsAt(0, 0);
+
+  wtc.removePointAt(0, 0);
 
   wtc.printSpec(0);
 }
