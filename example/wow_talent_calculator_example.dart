ee42af97e545f2821a905b11f8d35fa2eda26d67
Alexander Schellenberg <oppahansi@gmail.com>
2021-11-05 15:13:23 +0100

    Improved the example.

--- example/wow_talent_calculator_example.dart
+++ example/wow_talent_calculator_example.dart
@@ -1,22 +1,95 @@
 import 'package:wow_talent_calculator/wow_talent_calculator.dart';
 
 void main() {
-  var wtc = WowTalentCalculator(expansionId: 0, charClassId: 0);
+  var wtc = WowTalentCalculator(expansionId: 0, charClassId: 8);
 
-  for (int i = 0; i < 4; i++) {
+  // Standart DW Fury
+
+  // Read Additional Info in the package readme for all expansion and
+  // character class ids.
+
+  // expansionId: 0 -> Vanilla / Classic
+  // charClassId: 8 -> Warrior
+
+  // specId 0
+  // Arms (17)
+  for (int i = 0; i < 2; i++) {
     wtc.investPointAt(0, 0);
   }
 
-  wtc.investPointAt(0, 1);
-  wtc.investPointAt(0, 2);
-
   for (int i = 0; i < 3; i++) {
-    wtc.investPointAt(0, 4);
+    wtc.investPointAt(0, 2);
   }
 
-  wtc.investPointAt(0, 0);
-  wtc.investPointAt(0, 8);
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(0, 5);
+  }
 
-  wtc.printSpec(0);
+  wtc.investPointAt(0, 8);
+  wtc.investPointAt(0, 9);
+
+  for (int i = 0; i < 3; i++) {
+    wtc.investPointAt(0, 10);
+  }
+
+  for (int i = 0; i < 2; i++) {
+    wtc.investPointAt(0, 14);
+  }
+
+  // specId 1
+  // Fury (34)
+
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(1, 2);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(1, 6);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(1, 11);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(1, 12);
+  }
+
+  for (int i = 0; i < 2; i++) {
+    wtc.investPointAt(1, 13);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(1, 14);
+  }
+
+  wtc.investPointAt(1, 17);
+
+  for (int i = 0; i < 5; i++) {
+    wtc.investPointAt(1, 22);
+  }
+
+  wtc.investPointAt(1, 25);
+
   wtc.printAllSpecs();
+
+  /**
+   * Expected console output:
+    __________________________________________
+    | 2  0  3    ||    0  5    ||    0  0    |
+    |       |    ||            ||    |       |
+    | 0  5  |  0 ||    0  5    || 0  |  0  0 |
+    |    |  |    ||            || |  |       |
+    | 1  1  3    || 0  0  0  5 || 0  0  0  0 |
+    |       |    ||            ||            |
+    |    0  2    || 5  2  5    || 0  0  0    |
+    |            ||       |    ||            |
+    | 0  0  0  0 || 0  1  |  0 || 0  0  0    |
+    |    |       ||    |  |    ||    |       |
+    | 0  |  0    || 0  |  5    ||    |  0    |
+    |    |       ||    |       ||    |       |
+    |    0       ||    1       ||    0       |
+    __________________________________________
+
+   */
 }
